687c34c878258ce4a005a5ee4b9fe3fa88cc581a
prabhu <prabhu369b@gmail.com>
2024-04-23 14:48:28 +0530

    f

---
+++ src/components/pages/contact/contact.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+
+
+function Contact(){
+    return(
+        <>
+            <h1>Contact</h1>
+            <Link to={"/"}><button>Home</button></Link>
+        </>
+    )
+}
+export default Contact
